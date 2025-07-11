fd70545fc183640a3ae500b20658192f0b3d44c5
amunibf <amunibf@gmail.com>
2025-07-11 19:10:55 +0700

    Initial commit of the project

--- app.py
+++ app.py
@@ -367,5 +367,5 @@ if __name__ == '__main__':
     # Untuk produksi, gunakan server WSGI seperti Gunicorn atau uWSGI.
     # 'debug=False' harus selalu diatur di produksi.
     # 'use_reloader=False' penting saat menggunakan scheduler untuk menghindari tugas ganda.
-    # app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    # app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
